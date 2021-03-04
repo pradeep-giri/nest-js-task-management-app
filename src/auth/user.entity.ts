63a6ae0aee657d5ad47d6d72c4db75f5225e92d2
Pradeep Giri <pradeepgiri.pg123@gmail.com>
2021-03-04 23:43:23 +0530

    JWT added

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,22 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  salt: string;
+
+  @Column()
+  password: string;
+
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
+}
